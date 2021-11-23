9923dbee6b625a700fc9a8ffcc6c1b19f5f3cd9e
Daniel Barboza da Silva <daniel80barboza@gmail.com>
2021-11-23 00:48:32 -0300

    refactoring provider - finish - like product and like provider

--- api/mappers/mappers.client.js
+++ api/mappers/mappers.client.js
@@ -17,6 +17,7 @@ const toDTO = (model) => {
 
 const toDTOListLikeProviderProduct = (model) => {
   return {
+    id: model._id,
     product: {
       id: model._id,
       name: model.name,
@@ -53,31 +54,29 @@ const toDTOListLikeProviderProduct = (model) => {
 };
 
 const toDTOListLikeClientProvider = (model) => {
-  const { _id, provider, client } = model;
-
   return {
-    id: _id,
+    id: model._id,
     provider: {
-      id: provider._id,
-      cnpj: provider.cnpj,
-      fantasy_name: provider.fantasy_name,
-      social_name: provider.social_name,
-      address: provider.address,
-      uf: provider.uf,
-      city: provider.city,
-      responsible: provider.responsible,
-      phone: provider.phone,
-      status: provider.status,
+      id: model.provider._id,
+      cnpj: model.provider.cnpj,
+      fantasy_name: model.provider.fantasy_name,
+      social_name: model.provider.social_name,
+      address: model.provider.address,
+      uf: model.provider.uf,
+      city: model.provider.city,
+      responsible: model.provider.responsible,
+      phone: model.provider.phone,
+      status: model.provider.status,
     },
     client: {
-      id: client._id,
-      first_name: client.first_name,
-      last_name: client.last_name,
-      birth_date: client.birth_date,
-      phone: client.phone,
-      uf: client.uf,
-      city: client.city,
-      status: client.status,
+      id: model.client._id,
+      first_name: model.client.first_name,
+      last_name: model.client.last_name,
+      birth_date: model.client.birth_date,
+      phone: model.client.phone,
+      uf: model.client.uf,
+      city: model.client.city,
+      status: model.client.status,
     },
   };
 };
--- api/services/services.like.js
+++ api/services/services.like.js
@@ -1,130 +1,8 @@
-const {
-  provider,
-  product,
-  client,
-  like,
-  category,
-} = require('../models/models.index');
+const { provider, product, client, like } = require('../models/models.index');
 const {
   toDTOListLikeClientProvider,
   toDTOListLikeProviderProduct,
 } = require('../mappers/mappers.client');
-
-const ServiceSearchLikeClientProvider = async (provider_id, client_id) => {
-  const [providerDB, clientDB, likeDB] = await Promise.all([
-    provider.findById(provider_id),
-    client.findById(client_id),
-    like.findOne({ provider: provider_id, client: client_id }),
-  ]);
-
-  if (!providerDB) {
-    return {
-      success: false,
-      details: 'The provider informed does not exist!',
-    };
-  } else if (!clientDB) {
-    return {
-      success: false,
-      details: 'The client informed does not exist!',
-    };
-  } else if (!likeDB) {
-    return {
-      success: false,
-      details: 'No likes found!',
-    };
-  } else if (likeDB) {
-    return {
-      success: true,
-      message: 'Operation performed successfully!',
-      data: [toDTOListLikeClientProvider(...likeDB)],
-    };
-  }
-};
-
-const ServiceCreateLikeClientProvider = async (provider_id, client_id) => {
-  const [providerDB, clientDB, likeDB] = await Promise.all([
-    provider.findById(provider_id),
-    client.findById(client_id),
-    like.findOne({ provider: provider_id, client: client_id }),
-  ]);
-
-  if (!providerDB) {
-    return {
-      success: false,
-      details: 'The provider informed does not exist!',
-    };
-  } else if (!clientDB) {
-    return {
-      success: false,
-      details: 'The client informed does not exist!',
-    };
-  } else if (likeDB) {
-    return {
-      success: false,
-      details: 'Have you already enjoyed this provider!',
-    };
-  } else if (!likeDB) {
-    const resp = await like.create({
-      provider: provider_id,
-      client: client_id,
-    });
-
-    const result_like = await Promise.all([resp.save()]);
-
-    if (!result_like) {
-      return {
-        success: true,
-        data: {
-          message: 'Successfully liked!',
-          id: resp._id,
-          provider: resp.provider,
-          client: resp.client,
-        },
-      };
-    } else {
-      return {
-        success: false,
-        details: 'Error performing like!',
-      };
-    }
-  }
-};
-
-const ServiceRemoveLikeClientProvider = async (provider_id, client_id) => {
-  const [providerDB, clientDB, likeDB] = await Promise.all([
-    provider.findById(provider_id),
-    client.findById(client_id),
-    like.findOne({ provider: provider_id, client: client_id }),
-  ]);
-
-  if (!providerDB) {
-    return {
-      success: false,
-      details: 'The provider informed does not exist!',
-    };
-  } else if (!clientDB) {
-    return {
-      success: false,
-      details: 'The client informed does not exist!',
-    };
-  } else if (likeDB) {
-    const result_like = await Promise.all([like.deleteOne()]);
-
-    if (result_like) {
-      return {
-        success: true,
-        data: {
-          message: 'like removed successfully!',
-        },
-      };
-    } else {
-      return {
-        success: false,
-        details: 'There is no like!',
-      };
-    }
-  }
-};
 
 const ServiceSearchLikeProviderProduct = async (provider_id) => {
   const likeDB = await like
@@ -141,15 +19,15 @@ const ServiceSearchLikeProviderProduct = async (provider_id) => {
     .populate('product')
     .populate('category')
     .populate('provider');
-
-  if (!likeDB) {
+  if (resultDB == 0) {
     return {
       success: false,
       details: 'No likes found!',
     };
-  } else if (likeDB) {
+  } else if (resultDB !== 0) {
     return {
       success: true,
+      message: 'Operation performed successfully!',
       data: resultDB.map((item) => {
         return toDTOListLikeProviderProduct(item);
       }),
@@ -189,8 +67,8 @@ const ServiceCreateLikeProviderProduct = async (provider_id, product_id) => {
     if (result_like) {
       return {
         success: true,
+        message: 'Successfully liked!',
         data: {
-          message: 'Successfully liked!',
           id: resp._id,
           provider: resp.provider,
           product: resp.product,
@@ -223,7 +101,119 @@ const ServiceRemoveLikeProviderProduct = async (provider_id, product_id) => {
       details: 'The product informed does not exist!',
     };
   } else if (likeDB) {
-    const result_like = await Promise.all([like.deleteOne()]);
+    const result_like = await Promise.all([like.deleteOne(likeDB)]);
+    if (result_like) {
+      return {
+        success: true,
+        data: {
+          message: 'like removed successfully!',
+        },
+      };
+    } else {
+      return {
+        success: false,
+        details: 'There is no like!',
+      };
+    }
+  }
+};
+
+const ServiceSearchLikeClientProvider = async (client_id) => {
+  const resultLikeDB = await like
+    .find({
+      client: client_id,
+    })
+    .where('client')
+    .ne(null)
+    .populate('provider');
+
+  const resultDB = await like
+    .find({ provider: resultLikeDB.provider, client: client_id })
+    .populate('provider')
+    .populate('client');
+  if (resultDB == 0) {
+    return {
+      success: false,
+      details: 'No likes found!',
+    };
+  } else if (resultDB !== 0) {
+    return {
+      success: true,
+      message: 'Operation performed successfully!',
+      data: resultDB.map((item) => {
+        return toDTOListLikeClientProvider(item);
+      }),
+    };
+  }
+};
+
+const ServiceCreateLikeClientProvider = async (provider_id, product_id) => {
+  const [providerDB, productDB, likeDB] = await Promise.all([
+    provider.findById(provider_id),
+    product.findById(product_id),
+    like.findOne({ provider: provider_id, product: product_id }),
+  ]);
+
+  if (!providerDB) {
+    return {
+      success: false,
+      details: 'The provider informed does not exist!',
+    };
+  } else if (!productDB) {
+    return {
+      success: false,
+      details: 'The product informed does not exist!',
+    };
+  } else if (likeDB) {
+    return {
+      success: false,
+      details: 'The provider has already liked the product!',
+    };
+  } else if (!likeDB) {
+    const resp = await like.create({
+      provider: provider_id,
+      product: product_id,
+    });
+
+    const result_like = await Promise.all([resp.save()]);
+    if (result_like) {
+      return {
+        success: true,
+        message: 'Successfully liked!',
+        data: {
+          id: resp._id,
+          provider: resp.provider,
+          product: resp.product,
+        },
+      };
+    } else {
+      return {
+        success: false,
+        details: 'There is no like!',
+      };
+    }
+  }
+};
+
+const ServiceRemoveLikeClientProvider = async (provider_id, product_id) => {
+  const [providerDB, productDB, likeDB] = await Promise.all([
+    provider.findById(provider_id),
+    product.findById(product_id),
+    like.findOne({ provider: provider_id, product: product_id }),
+  ]);
+
+  if (!providerDB) {
+    return {
+      success: false,
+      details: 'The provider informed does not exist!',
+    };
+  } else if (!productDB) {
+    return {
+      success: false,
+      details: 'The product informed does not exist!',
+    };
+  } else if (likeDB) {
+    const result_like = await Promise.all([like.deleteOne(likeDB)]);
     if (result_like) {
       return {
         success: true,
