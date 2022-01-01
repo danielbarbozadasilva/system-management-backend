8077190c4d23eb22d0773c0fd94eba5f4c8ca0a3
Daniel Barboza da Silva <daniel80barboza@gmail.com>
2022-01-01 16:04:11 -0300

    fix: client model validation

--- api/models/models.client.js
+++ api/models/models.client.js
@@ -4,42 +4,27 @@ const Schema = mongoose.Schema;
 const clientSchema = {
   first_name: {
     type: String,
-    uppercase: true,
-    trim: true,
-    maxlength: 50,
-    minlength: 5,
     required: true,
   },
   last_name: {
     type: String,
     required: true,
-    uppercase: true,
-    trim: true,
-    maxlength: 50,
-    minlength: 5,
-    required: true,
   },
   birth_date: {
     type: Date,
     required: true,
-    trim: true,
   },
   phone: {
     type: String,
     required: true,
-    trim: true,
   },
   uf: {
     type: String,
     required: true,
-    uppercase: true,
-    trim: true,
   },
   city: {
     type: String,
     required: true,
-    uppercase: true,
-    trim: true,
   },
   status: {
     type: String,
