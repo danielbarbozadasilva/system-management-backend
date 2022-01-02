3c29c4356abd96282f32f48a454071270753c07a
Daniel Barboza da Silva <daniel80barboza@gmail.com>
2022-01-02 15:29:24 -0300

    refactor: controller user

--- api/controllers/controllers.user.js
+++ api/controllers/controllers.user.js
@@ -1,14 +1,14 @@
-const userService = require("../services/services.user");
+const userService = require('../services/services.user')
 
-const ControllerAuth = async (req, res, next) => {
-  const { email, password } = req.body;
-  const resultService = await userService.ServiceAuthenticate(email, password);
-   const code = resultService.success ? 200 : 401;
-   const message = resultService.success
-     ? { message: resultService.message }
-     : { details: resultService.details };
-   const data = resultService.data ? resultService.data : '';
-   return res.status(code).send({ message: message, data });
-};
+const authController = async (req, res) => {
+  const { email, password } = req.body
+  const resultService = await userService.authenticateService(email, password)
+  const code = resultService.success ? 200 : 401
+  const message = resultService.success
+    ? { message: resultService.message }
+    : { details: resultService.details }
+  const data = resultService.data ? resultService.data : ''
+  return res.status(code).send({ message, data })
+}
 
-module.exports = { ControllerAuth };
+module.exports = { authController }
