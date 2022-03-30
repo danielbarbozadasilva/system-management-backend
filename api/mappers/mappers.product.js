281c912f5ce0f4d250221c599c17f1690c19bc7b
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-03-30 00:39:16 -0300

    refactor: update provider mapper

--- api/mappers/mappers.product.js
+++ api/mappers/mappers.product.js
@@ -1,57 +1,51 @@
 const fileUtils = require('../utils/utils.file')
 
 const toDTO = (model) => {
-  const { _id, name, description, price, provider, category, image } = model
   return {
-    id: _id,
-    name,
-    description,
-    price: parseFloat(price).toLocaleString('pt-br', {
+    id: model._id,
+    name: model.name,
+    description: model.description,
+    price: parseFloat(model.price).toLocaleString('pt-br', {
       style: 'currency',
       currency: 'BRL'
     }),
-    image: fileUtils.UtilCreateAddressDownload('product', image.name),
-    category,
-    provider
+    image: fileUtils.UtilCreateAddressDownload('product', model.image.name),
+    categoryName: model.result_category.map((item) => {
+      return item?.name
+    }),
+    provider: model.provider,
+    result_likes: model.result_likes.map((item) => {
+      return {
+        id: item?._id,
+        product: item?.product,
+        provider: item?.provider
+      }
+    })
   }
 }
 
 const toItemListDTO = (model) => {
-  const { _id, name, description, price, provider, category, image } = model
-  const {
-    id,
-    cnpj,
-    fantasyName,
-    socialName,
-    address,
-    uf,
-    city,
-    responsible,
-    phone,
-    status
-  } = provider
-
   return {
-    id: _id,
-    category,
-    name,
-    description,
-    price: parseFloat(price).toLocaleString('pt-br', {
+    id: model._id,
+    category: model.category,
+    name: model.name,
+    description: model.description,
+    price: parseFloat(model.price).toLocaleString('pt-br', {
       style: 'currency',
       currency: 'BRL'
     }),
-    image: fileUtils.UtilCreateAddressDownload('product', image.name),
+    image: fileUtils.UtilCreateAddressDownload('product', model.image.name),
     provider: {
-      id: _id,
-      cnpj,
-      fantasyName,
-      socialName,
-      address,
-      uf,
-      city,
-      responsible,
-      phone,
-      status
+      id: model.provider._id,
+      cnpj: model.provider.cnpj,
+      fantasyName: model.provider.fantasyName,
+      socialName: model.provider.socialName,
+      address: model.provider.address,
+      uf: model.provider.uf,
+      city: model.provider.city,
+      responsible: model.provider.responsible,
+      phone: model.provider.phone,
+      status: model.provider.status
     }
   }
 }
