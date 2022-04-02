afbc3805b9fee332f0645fd1508b13d5a9ba5dcf
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-04-02 20:59:22 -0300

    hotfix: fix display username

--- api/mappers/mappers.user.js
+++ api/mappers/mappers.user.js
@@ -12,14 +12,14 @@ const VerifyTypeUser = (type) => {
     default:
       break
   }
-};
+}
 
 const toUserDTO = (model) => {
-  const { _id, email, kind, name, socialName } = model
+  const { _id, email, kind, name, socialName, firstName, lastName } = model
   return {
     id: _id,
     email,
-    name: name ? name : socialName,
+    name: name || socialName || firstName + ' ' + lastName,
     typeUser: VerifyTypeUser(kind)
   }
 }
