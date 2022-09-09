52c986d6001cc396b8450aac2ab9aa3e262d6991
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-09-09 14:50:17 -0300

    fix: permission error

--- api/utils/middlewares/middlewares.authorization.js
+++ api/utils/middlewares/middlewares.authorization.js
@@ -2,9 +2,10 @@ const { decodeToken } = require('../utils.cryptography')
 const { checkPermissionService } = require('../../services/services.user')
 
 const authorizationMiddleware = (permission) => async (req, res, next) => {
-  const { token } = req.headers
-  const { typeUser } = decodeToken(token)
   if (permission !== '*') {
+    const { token } = req.headers
+    const { typeUser } = decodeToken(token)
+
     checkPermissionService(typeUser, permission)
   }
   next()
