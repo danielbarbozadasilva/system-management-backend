6d061f7db0f02848c384c914709133b2d8c929e0
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2023-01-25 18:08:03 -0300

    chore: separate rules from users

--- api/services/services.user.js
+++ api/services/services.user.js
@@ -4,38 +4,7 @@ const userMapper = require('../mappers/mappers.user')
 const ErrorGeneric = require('../exceptions/erros.generic-error')
 const ErrorNotAuthorized = require('../exceptions/errors.user-not-authorized')
 const ErrorNotAuthenticated = require('../exceptions/errors.user-not-authenticated')
-
-const profile = [
-  {
-    type: 1,
-    description: 'admin',
-    permission: [
-      'CLIENT_SEARCH',
-      'CHANGE_STATUS_PROVIDER',
-      'CREATE_CATEGORY',
-      'UPDATE_CATEGORY',
-      'REMOVE_CATEGORY'
-    ]
-  },
-  {
-    type: 2,
-    description: 'provider',
-    permission: [
-      'SEARCH_PRODUCT',
-      'SEARCH_PRODUCT_ID',
-      'CREATE_PRODUCT',
-      'REMOVE_PRODUCT',
-      'UPDATE_PRODUCT',
-      'CREATE_LIKE_PRODUCT',
-      'REMOVE_LIKE_PRODUCT'
-    ]
-  },
-  {
-    type: 3,
-    description: 'client',
-    permission: ['CLIENT_SEARCH_ID', 'CLIENT_LIKE_CREATE', 'CLIENT_LIKE_REMOVE']
-  }
-]
+const profile = require('../utils/utils.rules')
 
 const checkPermissionService = (type, permission) => {
   const result = profile.find((item) => item.type === type)
